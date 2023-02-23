639aa892387a62326f2447770f294b144e3d66df
AbrahamDevarana <99768357+AbrahamDevarana@users.noreply.github.com>
2023-02-23 16:30:37 -0600

    Fix Config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'https://abrahamdevarana.github.io/',
+    base: 'https://abrahamdevarana.github.io/gh-react-vite/',
     plugins: [react()],
 })
